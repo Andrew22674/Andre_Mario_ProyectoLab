3fb51db30bc38fff17c26b272b988106805df750
Andre V <avela@unitec.edu>
2017-03-06 12:15:56 -0600

    asdas

--- main.cpp
+++ main.cpp
@@ -2,6 +2,10 @@
 #include <string>
 #include <vector>
 #include <ctime>
+#include <string.h>
+#include <cstdarg>
+#include <fstream>
+
 
 #include "Consolas.h"
 #include "Juegos.h"
@@ -484,7 +488,22 @@ int main(){
           }
         }else if(opcionvendedor == 2){//Vender
           usuario_v -> vender();
-          cout << "Maria" << endl;
+
+          ofstream outfile;
+          time_t currentTime = time(0);
+          tm* currentDate = localtime(&currentTime);
+          char filename[256] = {0};
+
+          strcpy(filename, "./log_ventas/");
+          strcat(filename, fmt("%d:%d:%d_%d-%d-%d.log",
+                 currentDate->tm_hour, currentDate->tm_min, currentDate->tm_sec,
+                 currentDate->tm_mday, currentDate->tm_mon+1,
+                 currentDate->tm_year+1900).c_str());
+
+          outfile.open(filename, std::ios::app);
+          outfile << "Prueba";
+          outfile.close();
+
         }else if(opcionvendedor == 3){
           cout << endl;
         }else{
@@ -553,4 +572,25 @@ bool numSerieJuegos(vector<Juegos*> juegos, int numserie){
   }
 
   return ya_existe;
+}
+
+string log_ventas(const std::string& fmt, ...) {
+    int size = 200;
+    string str;
+    va_list ap;
+    while (1) {
+        str.resize(size);
+        va_start(ap, fmt);
+        int n = vsnprintf((char*)str.c_str(), size, fmt.c_str(), ap);
+        va_end(ap);
+        if (n > -1 && n < size) {
+            str.resize(n);
+            return str;
+        }
+        if (n > -1)
+            size = n + 1;
+        else
+            size *= 2;
+    }
+    return str;
 }
